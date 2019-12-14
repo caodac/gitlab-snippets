aff2cac0712781da88c0c8e4a21d63a4154fa283
trung <caodac@gmail.com>
2019-12-13 22:00:11 -0500

    rework for python3 and update for mouse

--- unigene.py
+++ unigene.py
@@ -2,8 +2,8 @@ import mysql.connector
 import sys
 
 USER='tcrd'
-HOST='tcrd.kmc.io'
-DB='tcrd540'
+HOST='tcrd.ncats.io'
+DB='tcrd610'
 TOTALS = {}
 
 def connect(db):
@@ -20,20 +20,20 @@ def parse_unigene_profiles(file):
     for line in f:
         if line[0] == '>':
             toks = line[1:].strip().split('|')
-            gene = long(toks[0])
+            gene = int(toks[0])
             dim = toks[1]
-            if not TOTALS.has_key(dim):
+            if not dim in TOTALS:
                 TOTALS[dim] = {}
-            if not genes.has_key(gene):
+            if not gene in genes:
                 genes[gene] = {}
-            if not genes[gene].has_key(dim):
+            if not dim in genes[gene]:
                 genes[gene][dim] = {}
             #print "<{}><{}>".format(gene, dim)
         else:
             toks = line.split('\t')
             counts = toks[1].split('/')
             total = int(counts[1])
-            if not TOTALS[dim].has_key(toks[0]):
+            if not toks[0] in TOTALS[dim]:
                 TOTALS[dim][toks[0]] = total
             elif total != TOTALS[dim][toks[0]]:
                 sys.stderr.write("{} != {} for {}\n"
@@ -46,47 +46,49 @@ def parse_unigene_profiles(file):
     
 def targets_for_unigene(db, genes):
     cursor = db.cursor(dictionary = True, buffered=True)
-    query = ("select c.tdl,b.uniprot,c.fam,b.sym,a.value as unigene,c.name "
-             "from xref a, protein b, target c, t2tc d "
-             "where a.protein_id = b.id and a.xtype='UniGene' "
-             "and b.id = d.protein_id and c.id = d.target_id")
+    query = """select c.tdl,b.uniprot,c.fam,b.sym as human_gene, 
+a.symbol as mouse_gene,a.geneid as mouse_gene_id
+from protein b, target c, t2tc d 
+left join ortholog a on d.protein_id=a.protein_id and a.species='mouse'
+where b.id = d.protein_id and c.id = d.target_id"""
 
-    print "UniProt\tGene\tFamily\tTDL\tName\tUniGene",
-    dims = ['Developmental Stage','Body Sites','Health State']
+    print ("UniProt\tHumanGene\tFamily\tTDL\tMouseGene\tMouseGeneID",end='')
+#    dims = ['Developmental Stage','Body Sites','Health State']
+    dims = ['Developmental Stage','Body Sites']
     for d in dims:
+        sorted(TOTALS[d])
         vals = TOTALS[d].keys()
-        vals.sort()
         for v in vals:
-            print "\t{}: {} ({})".format(d, v, TOTALS[d][v]),
-    print
+            print ("\t{}: {} ({})".format(d, v, TOTALS[d][v]), end='')
+    print ('')
     try:
         cursor.execute(query)
         seen = {}
         for result in cursor:
-            unigene = result['unigene']
-            sym = result['sym']
+            unigene = result['mouse_gene']
+            gene = result['mouse_gene_id']
+            sym = result['human_gene']
             if None != unigene: # and not seen.has_key(sym):
                 fam = result['fam']
                 if None == fam:
                     fam = 'Unknown'
                     
-                gene = long(unigene.split('.')[1])
-                if genes.has_key(gene):
-                    print result['uniprot'],"\t",sym,"\t",
-                    print fam,"\t",result['tdl'],"\t",
-                    print result['name'],"\t",unigene,
+                #gene = int(unigene.split('.')[1])
+                if gene in genes:
+                    print ('%s\t"%s"\t%s\t%s\t"%s"\t%d' %
+                           (result['uniprot'],sym,fam,result['tdl'],
+                            unigene,gene), end='')
                     for d in dims:
+                        sorted(TOTALS[d])
                         vals = TOTALS[d].keys()
-                        vals.sort()
                         for v in vals:
-                            if genes[gene][d].has_key(v):
-                                print "\t{}".format(genes[gene][d][v]),
+                            if v in genes[gene][d]:
+                                print ("\t{}".format(genes[gene][d][v]),end='')
                             else:
-                                print "\t",
-                    print
+                                print ("\t",end='')
+                    print ('')
                 else:
-                    sys.stderr.write("No unigene EST profile for {}!\n"
-                                     .format(gene))
+                    print("No unigene EST profile for %s!" % (gene), file=sys.stderr)
                 seen[sym] = unigene
     except ValueError:
         pass
@@ -94,7 +96,7 @@ def targets_for_unigene(db, genes):
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
-        print "usage: unigene.py Hs.profiles"
+        print ("usage: unigene.py Mm.profiles")
         sys.exit(1)
 
     genes = parse_unigene_profiles(sys.argv[1])
